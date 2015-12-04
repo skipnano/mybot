84690f9e0f97be6203d6f9f619fef557f185b258
Dean Gilroy <dean.gilroy@gmail.com>
2015-12-03 22:50:51 -0500

    Added stories to code for context

--- sprintbot.js
+++ sprintbot.js
@@ -1,11 +1,21 @@
-/*OPTION ONE*/
+/*Sprint Bot*/
 
-var SprintData = { [devName: 'Ben', assigned: 20, completed: 16] , [devName: 'Julia', assigned: 30, completed: 25], [devName: 'Charlie', assigned: 15, completed: 5]};
+/*
+OPTION 1
+Create an object with nested arrays with Sprint data for three developers (devName, assigned, completed, inProgress)
+Initialize hubot
+Create a function to calculate the percentage of action items completed
+if(above 70% return msg "you're % complete! Great job!") 
+else if(below 70% return msg "you're % complete! How can we help?")
+else("Are you OK?")
+*/
+
+var SprintData = { [devName: 'Ben', assigned: 20, completed: 16] , [devName: 'Julia', assigned: 30, completed: 25], [devName: 'Charlie', assigned: 15, completed: 5]}
 
 module.exports = function(robot) {
 	robot.respond(/How's (.*)'s sprint?/i, function(msg) {
-	// stuff goes here
-	});
+
+	})
 }
 
 function progress(ais) {
@@ -13,22 +23,27 @@ function progress(ais) {
 	return completedAI;
 }
 
-
-/*OPTION TWO*/
+/*
+OPTION 2
+Create an object constructor for three devs sprint data (assigned, completed, inProgress)
+Initialize hubot
+Create a function to calculate the percentage of action items completed
+*/
 
 var SprintData = function(assigned, completed, inProgress) {
 	this.assigned = assigned;
 	this.completed = completed;
 	this.inProgress = inProgress;
 }
+
 var ben = new SprintData( assigned: 20, completed: 16, inProgress: 2 );
 var julia = new SprintData( assigned: 30, completed: 25, inProgress: 3 );
 var charlie = new SprintData( assigned: 15, completed: 5, inProgress: 1 );
 
 module.exports = function(robot) {
 	robot.respond(/progress for (.*)/i, function(msg) {
-	// stuff here
-	});
+
+	})
 }
 
 function progress(ais) {
