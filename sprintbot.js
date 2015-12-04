2526b803c9d8afc6ee81042100837808db33574c
Dean Gilroy <dean.gilroy@gmail.com>
2015-12-03 22:44:45 -0500

    added correct code

--- sprintbot.js
+++ sprintbot.js
@@ -1,25 +1,37 @@
-/*Convert USD to BTC */
+/*OPTION ONE*/
 
-var btcRate = 300;
+var SprintData = { [devName: 'Ben', assigned: 20, completed: 16] , [devName: 'Julia', assigned: 30, completed: 25], [devName: 'Charlie', assigned: 15, completed: 5]};
 
 module.exports = function(robot) {
-	robot.respond(/convert (.*) to btc/i, function(msg) {
-		//conditionals, for loops can be written here
-		var userUSD = msg.match[1];
-		userUSD = parseFloat(userUSD);
-		if(userUSD && userUSD > 0) {
-			var btc = usdToBTC(userUSD);
-			// how a messgase is sent back to slackbot
-			return msg.send(btc + ' btc :moneybag:');
-		} else {
-			return msg.send('We can only convert numbers. :rage:');
-		}
+	robot.respond(/How's (.*)'s sprint?/i, function(msg) {
+	// stuff goes here
 	});
-	//this is where the next bot function would go - robot.respond()
-};
+}
+
+function progress(ais) {
+	var completedAI = this.assigned / this.completed;
+	return completedAI;
+}
 
 
-function usdToBTC(usd) {
-	var convertedBTC = usd / btcRate;
-	return convertedBTC;
+/*OPTION TWO*/
+
+var SprintData = function(assigned, completed, inProgress) {
+	this.assigned = assigned;
+	this.completed = completed;
+	this.inProgress = inProgress;
+}
+var ben = new SprintData( assigned: 20, completed: 16, inProgress: 2 );
+var julia = new SprintData( assigned: 30, completed: 25, inProgress: 3 );
+var charlie = new SprintData( assigned: 15, completed: 5, inProgress: 1 );
+
+module.exports = function(robot) {
+	robot.respond(/progress for (.*)/i, function(msg) {
+	// stuff here
+	});
+}
+
+function progress(ais) {
+	var completedAI = this.assigned / this.completed;
+	return completedAI; 
 }
