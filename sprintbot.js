0bb04bf0e065999d2913c495b4d2080e0e005728
Dean Gilroy <dean.gilroy@gmail.com>
2015-12-05 20:54:25 -0500

    Updates options 1 and 2 based on feedback

--- sprintbot.js
+++ sprintbot.js
@@ -2,51 +2,92 @@
 
 /*
 OPTION 1
-Create an object with nested arrays with Sprint data for three developers (devName, assigned, completed, inProgress)
+Create an object of sprint data for three Developers where each developer is a key
 Initialize hubot
-Create a function to calculate the percentage of action items completed
-if(above 70% return msg "you're % complete! Great job!") 
-else if(below 70% return msg "you're % complete! How can we help?")
-else("Are you OK?")
+Create a function to calculate the progress for a developer (percent of stories completed)
 */
 
-var SprintData = { [devName: 'Ben', assigned: 20, completed: 16] , [devName: 'Julia', assigned: 30, completed: 25], [devName: 'Charlie', assigned: 15, completed: 5]}
+var devlopers = { 
+	ben: {
+		name: 'Ben',
+		assigned: 20, 
+		completed: 16
+	},
+	julia: {
+		name: 'Julia', 
+		assigned: 30, 
+		completed: 25
+	}, 
+	charlie: {
+		name: 'Charlie',
+		assigned: 15, 
+		completed: 5
+	}
+};
 
 module.exports = function(robot) {
-	robot.respond(/How's (.*)'s sprint?/i, function(msg) {
+	robot.respond(/sprint progress for (.*)/i, function(msg) {
+		 var devName = msg.match[1];
+		 if(devName === 'Ben') {
+		 	var progress = progressCalc(devName);
+		 	return msg.send(devName + "/'s progress is " + progress);
+		 } else {
+		 	return msg.send("Stop making up names :sadface:");
+		 }
 
-	})
+	});
 }
 
-function progress(ais) {
-	var completedAI = this.assigned / this.completed;
+function progressCalc(ais) {
+	var completedAI = (this.completed / this.assigned)*100;
 	return completedAI;
 }
 
 /*
 OPTION 2
-Create an object constructor for three devs sprint data (assigned, completed, inProgress)
+Create an object constructor for three developers' sprint data (name, assigned, completed, inProgress)
 Initialize hubot
-Create a function to calculate the percentage of action items completed
+Create a function to calculate the progress of the developer's sprint
+If the user inputs a valid developer name, return that dev's sprint progress
 */
 
-var SprintData = function(assigned, completed, inProgress) {
+var Developer = function(name, assigned, completed, inProgress) {
+	this.name = name;
 	this.assigned = assigned;
 	this.completed = completed;
 	this.inProgress = inProgress;
 }
 
-var ben = new SprintData( assigned: 20, completed: 16, inProgress: 2 );
-var julia = new SprintData( assigned: 30, completed: 25, inProgress: 3 );
-var charlie = new SprintData( assigned: 15, completed: 5, inProgress: 1 );
-
-module.exports = function(robot) {
-	robot.respond(/progress for (.*)/i, function(msg) {
-
-	})
+function.prototype.progress = function() {
+	return (this.completed / this.assigned)*100;
 }
 
-function progress(ais) {
-	var completedAI = this.assigned / this.completed;
-	return completedAI; 
+var ben = new Developer( 'Ben', 20, 16, 2 );
+var julia = new Developer( 'Julia', 30, 25, 3 );
+var charlie = new Developer( 'Charlie', 15, 5, 1 );
+
+var developers = [
+	ben: ben,
+	julia: julia,
+	charlie: charlie;
+];
+
+module.exports = function(robot) {
+	robot.respond(/sprint progress for (.*)/i, function(msg) {
+
+		var devName = msg.match[1];
+		if(developers[0]){
+			var progResult = developers['ben'].progress();
+			return msg.send(devName + "/'s progress is " + progResult);
+		} else if(developers[1]) {
+			var progResult = developers['julia'].progress();
+			return msg.send(devName + "/'s progress is " + progResult);
+		} else if(developers[2]) {
+			var progResult = developers['charlie'].progress();
+			return msg.send(devName + "/'s progress is " + progResult);)
+		} else {
+			return ('Stop making up names :sadface;');
+		}
+
+	});
 }
