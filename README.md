556504e56d361f9e40590de03cf8f720a57d2b6a
Dean Gilroy <dean.gilroy@gmail.com>
2015-12-08 22:05:09 -0500

    added readme file, and latest iteration of sprintbot js

---
+++ README.md
@@ -0,0 +1,18 @@
+Project: Sprintbot
+Developed by Dean Gilroy
+Javascript Alpha Class
+
+This bot will return sprint metrics when a developer's name is querued. Currently there are two queries that a user can ask the bot.
+In the future this bot can be hooked up to Trello's API to get the kanban data, or other proprietary devOps API.
+
+The data referenced by the bot is created using an object constructor called Developers.
+
+The first query calculates the sprint progress for a developer in the function progress() by dividing the (#completed/#assigned) / 100.
+The result will be a percent. The bot will respond with the developer's name and their progress
+
+The second query will return the number or stories in progress for the developer queried. The bot takes the user input (developer name) and references the Developer object for that developer name.
+
+Roadmap:
+- Add additional capabilities such as sprint velocity, indicator of overall sprint progress (all devs) that throws a thumbs up if sprint is in good shape, or thumbs down if it's in bad shape (those indicators will need user definition)
+- Conditionals that will return a message if the developer name queried does not exist
+- Hook up to an API
--- sprintbot.js
+++ sprintbot.js
@@ -1,51 +1,6 @@
 /*Sprint Bot*/
 
 /*
-OPTION 1
-Create an object of sprint data for three Developers where each developer is a key
-Initialize hubot
-Create a function to calculate the progress for a developer (percent of stories completed)
-*/
-
-var devlopers = { 
-	ben: {
-		name: 'Ben',
-		assigned: 20, 
-		completed: 16
-	},
-	julia: {
-		name: 'Julia', 
-		assigned: 30, 
-		completed: 25
-	}, 
-	charlie: {
-		name: 'Charlie',
-		assigned: 15, 
-		completed: 5
-	}
-};
-
-module.exports = function(robot) {
-	robot.respond(/sprint progress for (.*)/i, function(msg) {
-		 var devName = msg.match[1];
-		 if(devName === 'Ben') {
-		 	var progress = progressCalc(devName);
-		 	return msg.send(devName + "/'s progress is " + progress);
-		 } else {
-		 	return msg.send("Stop making up names :sadface:");
-		 }
-
-	});
-}
-
-function progressCalc(ais) {
-	var completedAI = (this.completed / this.assigned)*100;
-	return completedAI;
-}
-
-//*Sprint Bot*/
-
-/*
 OPTION 2
 Create an object constructor for three developers' sprint data (name, assigned, completed, inProgress)
 Initialize hubot
@@ -68,21 +23,24 @@ var Developer = function(name, assigned, completed, inProgress) {
 var ben = new Developer( 'Ben', 20, 16, 2 );
 var julia = new Developer( 'Julia', 30, 25, 3 );
 var charlie = new Developer( 'Charlie', 15, 5, 1 );
+var anna = new Developer( 'Anna', 40, 19, 5);
 
 // This allows the bot to match a user input
 var developers = {
 	ben: ben,
 	julia: julia,
-	charlie: charlie
+	charlie: charlie,
+	anna: anna
 };
 
 module.exports = function(robot) {
+	// this function calculates the percentage of stories completed
 	robot.respond(/sprint progress for (.*)/i, function(msg) {
-		// msg.send("hello")
 		var devName = msg.match[1];
 		var developer = developers[devName];
 		msg.send(developer.name + "'s sprint is " + developer.progress() + "% complete");
 	});
+	// this function shows the number of stories in progress
 	robot.respond(/stories in progress for (.*)/i, function(msg) {
 		var devName = msg.match[1];
 		var developer = developers[devName];
